b9aae3fe121e70b0e089b348a5f122e27a46550d
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2023-07-18 20:30:13 +0300

    JS: functions added

--- main.js
+++ main.js
@@ -16,3 +16,64 @@ sign = ''; //знак
 finish = false;
 out.textContent = 0;
 }
+
+document.querySelector('.ac').onclick = clearAll;
+document.querySelector('.buttons').onclick = (event) => {
+    // нажата не кнопка
+    if(!event.target.classList.contains('btn')) return;
+     // нажата кнопка clearAll ac
+    if(event.target.classList.contains('ac')) return;
+
+    out.textContent = '';
+    // получаем нажатую кнопку
+    const key = event.target.textContent;
+
+    //если нажата клавиша 0-9 или .
+    if (digit.includes(key)) {
+        if(b === '' && sign === '') {
+        a += key;
+        console.log(a, b, sign);
+        out.textContent = a;
+        }
+        else if (a!== '' && b!=='' && finish) {
+
+        }
+        else {
+            b += key;
+            out.textContent = b;
+        }
+        console.log(a, b, sign);
+        return;
+    }
+    // если нажата клавиша + - / *
+    if(action.includes(key)) {
+    sign = key;
+    out.textContent = sign;
+    console.log(a, b, sign);
+    return;
+}
+
+// Нажата = 
+if(key === '=') {
+    switch (sign) {
+        case '+':
+            a = (+a) + (+b);
+            break;
+        case "-":
+            a = a - b;
+            break;
+        case "X":
+            a = a * b;
+            break;
+        case "/":
+            a = a / b;
+            break;
+    }
+    finish = true;
+    out.textContent = a;
+    console.log(a, b, sign);
+}
+
+
+}
+
